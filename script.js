44ef0945aee3a0dd72ffb9a0d6ad62d4386aa9ce
Angel Padilla <43829004+angeljadenpadilla@users.noreply.github.com>
2018-12-05 17:09:50 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".cartman").click(function() {
+    $(".lightning").slideToggle();
+});
+$(".wand").click(function() {
+    $(".fire").fadeToggle();
+});
+$(".hands").click(function() {
+    $(".water").toggle();
+});
+   
+
+  
