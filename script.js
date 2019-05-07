871e244a4cde85aeda74fd0f387eecd8f2eeb2ab
Angel Padilla <43829004+angeljadenpadilla@users.noreply.github.com>
2019-05-07 17:02:51 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ $(".wand").click(function() {
 $(".hands").click(function() {
     $(".water").toggle();
 });
-   
+$(".hands2").click(function() {
+    $(".smoke").toggle();
+});
+$(".hands3").click(function() {
+    $(".clone").slideToggle();
+});
+$(".hands4").click(function() {
+    $(".wind").fadeToggle();
+});
+
 
   
